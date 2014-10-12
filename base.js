97026bf6ca351320dba9cf788779ae2ca971e45a
Daniel <desiradaniel2007@hotmail.com>
2014-10-12 19:18:18 +0200

    Some demo additions

--- base.js
+++ base.js
@@ -1,5 +1,28 @@
-CCP.init({
-	id: 'circle',
-	color: 'rgb(255, 0, 0)',
-	width: 3
-});
+(function (document, CCP) {
+
+	'use strict';
+
+	var hideButton = document.getElementById('hide'),
+		showButton = document.getElementById('show');
+
+	CCP.init({
+		id: 'circle',
+		color: 'rgb(255, 0, 0)',
+		width: 3
+	});
+
+	hideButton.addEventListener('click', function _hide() {
+		hideButton.disabled = true;
+		showButton.disabled = false;
+
+		CCP.hide();
+	});
+
+	showButton.addEventListener('click', function _show() {
+		showButton.disabled = true;
+		hideButton.disabled = false;
+
+		CCP.show();
+	});
+
+})(document, CCP);
